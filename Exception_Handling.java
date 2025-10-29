a9f49961ffd67cbe570f39fc07ca1182ac57e639
ziad-mahmoud-moss <ziadmoussa429@gmail.com>
2025-10-29 19:59:57 +0300

    Update Exception_Handling.java

--- Exception_Handling.java
+++ Exception_Handling.java
@@ -1,57 +1,57 @@
 public class Exception_Handling {
     public static void main(String[] args)
     {
-        // This won't work because we are passing a string to an integer variable
-        // int myInt = Integer.parseInt("dido");
+        // This will fail because we're trying to convert a string into a number
+        // int myInt = Integer.parseInt("moss");
 
 
-        // Handling Exception using Try-Catch Method
+        // Example of handling an exception using try-catch
         // try {
         //     int myInt = Integer.parseInt("1");
         // }
         // catch(NumberFormatException nfe)
         // {
-        //     System.out.println("Cannot Print Because that's a string babe 😍");
+        //     System.out.println("Oops! That's a string, not a number ");
         // }
-        // System.out.println("End");
+        // System.out.println("Program finished successfully");
 
-        // Handling Exception using Default Uncaught Exception Handler
+        // Handling exception using the Default Uncaught Exception Handler
 
         // Thread thread = new Thread(new Runnable() {
         //     @Override
         //     public void run() {
-        //         int myInt = Integer.parseInt("dido");
+        //         int myInt = Integer.parseInt("moss");
         //     }
         // });
 
         // Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
         //     @Override
         //     public void uncaughtException(Thread t, Throwable e) {
-        //         System.out.println("Exception caught from thread: " + t.getName());
-        //         System.out.println("Exception message: " + e.getMessage());
+        //         System.out.println("Default Handler: Exception detected in thread: " + t.getName());
+        //         System.out.println("Default Handler: Error message: " + e.getMessage());
         //     }
         // });
 
         // thread.start();
-        // System.out.println("End of Main Thread");
+        // System.out.println("Main thread has ended");
 
-        // Handling Exception using Custom Uncaught Exception Handler
+        // Handling exception using a Custom Uncaught Exception Handler
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
-                int myInt = Integer.parseInt("dido");
+                int myInt = Integer.parseInt("moss");
             }
         });
 
         thread.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
             @Override
             public void uncaughtException(Thread t, Throwable e) {
-                System.out.println("Custom Handler: Exception caught from thread: " + t.getName());
-                System.out.println("Custom Handler: Exception message: " + e.getMessage());
+                System.out.println("Custom Handler: Error caught from thread: " + t.getName());
+                System.out.println("Custom Handler: Error details: " + e.getMessage());
             }
         });
 
         thread.start();
-        System.out.println("End of Main Thread");
+        System.out.println("Main thread execution completed");
     }
 }
